d5962b25f0e0f73d3466a206844efbcc3f06c26f
Blasevich Giovanni <37509154@telecomitalia.local>
2018-05-09 23:02:55 +0200

    setup inizialefinito

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('My Minimal React Webpack Babel Setup');
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import App from './App.jsx'
+//import data from './data/prova.json';
+
+ReactDOM.render(
+  <div>AA</div>,
+  document.getElementById('app')
+);
